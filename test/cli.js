15a702b866d927d9df22c6b66ab364aca431767e
Titus Wormer <tituswormer@gmail.com>
2018-06-10 22:33:36 +0200

    rehype-cli: update unified-args
    See [`unified-args#6.0.0`](https://github.com/unifiedjs/unified-args/releases/tag/6.0.0).

--- test/cli.js
+++ test/cli.js
@@ -14,7 +14,7 @@ test('rehype-cli', function (t) {
 
     st.plan(1);
 
-    execa.stderr(bin, ['--help']).then(function (result) {
+    execa.stdout(bin, ['--help']).then(function (result) {
       st.equal(
         result,
         [
@@ -68,7 +68,7 @@ test('rehype-cli', function (t) {
 
     st.plan(2);
 
-    execa.stderr(bin, ['--version']).then(function (result) {
+    execa.stdout(bin, ['--version']).then(function (result) {
       st.ok(
         /rehype: \d+\.\d+\.\d+/.test(result),
         'should include rehype version'
